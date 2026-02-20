d74bd9c1b5b8a34b358a79676caef4aff55c9a80
jamestharpert <jamestharpert@gmail.com>
2026-02-20 16:30:20 +0100

    Add Home component for TradeX Academy page

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1 style={{ fontSize: "42px" }}>TradeX Academy</h1>
+      <p>Aprende trading de forma profesional.</p>
+    </div>
+  );
+}
